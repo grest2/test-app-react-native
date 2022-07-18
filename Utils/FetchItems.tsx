89084bf1b7e4d2a62cc65ef715d2f8014e73538c
IKIS <polyanaozero@gmail.com>
2022-07-18 20:36:24 +0300

    feature/network: added component with fetching by get req

---
+++ Utils/FetchItems.tsx
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import {FlatList, ActivityIndicator, Text, View, ScrollView, Button, LogBox} from "react-native";
+
+export default class FetchItems extends Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = { dataSource: [] };
+    }
+
+    componentDidMount() {
+        return fetch("https://api.coincap.io/v2/assets")
+            .then((response) => response.json())
+            .then((json) => {
+
+                this.setState({
+                    dataSource: json.currencyes
+                })
+                console.log(this.state.dataSource)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        return (
+            <View>
+                <FlatList
+                    data={ this.state.dataSource }
+                    renderItem={this.state.renderItem}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            </View>
+        );
+    }
+}
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,13 +3,14 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import FetchItems from "../Utils/FetchItems";
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <FetchItems path="/Utils/FetchItems.tsx" />
     </View>
   );
 }
